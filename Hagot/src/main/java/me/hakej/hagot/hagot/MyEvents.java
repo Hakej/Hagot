dd1c45bc659d624bbab4f553c5015a69bfc7505a
Hakej <michaluszak97@gmail.com>
2019-09-14 08:41:27 +0200

    Add three basic events
    Add chatCheck, blockBreak and blockPlace event with some placeholder logic.

--- Hagot/src/main/java/me/hakej/hagot/hagot/MyEvents.java
+++ Hagot/src/main/java/me/hakej/hagot/hagot/MyEvents.java
@@ -1,16 +1,44 @@
 package me.hakej.hagot.hagot;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class MyEvents implements Listener {
 
+    private static final Material BANNED_MATERIAL = Material.TNT;
+
     @EventHandler
-    public void onMove(PlayerMoveEvent event) {
+    public void chatCheck(AsyncPlayerChatEvent event) {
+        String message = event.getMessage();
+        if (message.contains("wow")) {
+            System.out.println("Someone is astonished. Probably by this mod.");
+        }
+    }
+
+    @EventHandler
+    public void blockBreak(BlockBreakEvent event) {
+        Block block = event.getBlock();
+        Material material = block.getType();
         Player player = event.getPlayer();
-        player.sendMessage(ChatColor.GREEN + "You are moving..");
+        player.sendMessage(ChatColor.GRAY + "You have broke: " + ChatColor.YELLOW + material.name());
+    }
+
+    @EventHandler
+    public void blockPlace(BlockPlaceEvent event) {
+        Block block = event.getBlock();
+        Material material = block.getType();
+        Player player = event.getPlayer();
+
+        if (material.equals(BANNED_MATERIAL)) {
+            block.setType(Material.AIR);
+            player.sendMessage(ChatColor.RED + "You cannot place " + BANNED_MATERIAL + "!");
+        }
     }
 }
