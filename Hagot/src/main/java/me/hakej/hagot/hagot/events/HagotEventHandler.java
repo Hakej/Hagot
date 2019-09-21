240df4e55db1404ebeaac285b75cdece642505b0
Hakej <michaluszak97@gmail.com>
2019-09-21 09:59:45 +0200

    Change SpigotEventHandler
    Remove not implemented anywhere handle method without plugin argument
    Change name to HagotEventHandler

--- Hagot/src/main/java/me/hakej/hagot/hagot/events/HagotEventHandler.java
+++ Hagot/src/main/java/me/hakej/hagot/hagot/events/HagotEventHandler.java
@@ -3,11 +3,7 @@ package me.hakej.hagot.hagot.events;
 import org.apache.commons.lang.NotImplementedException;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public interface SpigotEventHandler {
-
-    static <T> void handle(T t) {
-        throw new NotImplementedException();
-    }
+public interface HagotEventHandler {
 
     static <T> void handle(JavaPlugin plugin, T t) {
         throw new NotImplementedException();
